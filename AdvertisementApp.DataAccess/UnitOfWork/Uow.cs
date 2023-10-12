5d37e3ad62323238ecdd95a368618e15ba04429d
KhanishAlasgarov <khanisha@code.edu.az>
2023-10-12 19:24:30 +0400

    Unit of work is ready

---
+++ AdvertisementApp.DataAccess/UnitOfWork/Uow.cs
@@ -0,0 +1,32 @@
+﻿using AdvertisementApp.DataAccess.Contexts;
+using AdvertisementApp.DataAccess.Interfaces;
+using AdvertisementApp.DataAccess.Repositories;
+using AdvertisementApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.DataAccess.UnitOfWork
+{
+    public class Uow : IUow
+    {
+        private readonly AdvertisementAppContext _context;
+
+        public Uow(AdvertisementAppContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<T> GetRepository<T>() where T : BaseEntity
+        {
+            return new Repository<T>(_context);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
