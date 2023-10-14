b347e4e8700d6315abc104263ca693ca86fd5fbc
KhanishAlasgarov <khanisha@code.edu.az>
2023-10-14 20:12:58 +0400

    Fluent Validation added to the project

---
+++ AdvertisementApp.Application/ValidationRules/ProvidedServiceCreateDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using AdvertisementApp.Dtos.ProvidedServiceDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Application.ValidationRules
+{
+    public class ProvidedServiceCreateDtoValidator:AbstractValidator<ProvidedServiceCreateDto>
+    {
+        public ProvidedServiceCreateDtoValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.ImagePath).NotEmpty().MaximumLength(500);
+            RuleFor(x => x.Title).NotEmpty().MaximumLength(500);
+        }
+    }
+}
---
+++ AdvertisementApp.Dtos/ProvidedServiceDtos/ProvidedServiceCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Dtos.ProvidedServiceDtos
+{
+    public class ProvidedServiceCreateDto
+    {
+        public string? Title { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Description { get; set; }
+    }
+}
